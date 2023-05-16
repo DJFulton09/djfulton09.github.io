a94ff304def57221f409776304eee86f73a4e5f6
DJFulton09 <121510708+DJFulton09@users.noreply.github.com>
2023-05-16 09:39:11 -0700

    made changes

---
+++ js/portfolio.js
@@ -0,0 +1,12 @@
+// this functions open and close the contact form
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+// this function displays the first image in the slideshow when the page loads
+var slideIndex = 1;
+showSlides(slideIndex);
